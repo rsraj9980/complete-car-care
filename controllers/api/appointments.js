d277e2fe746334ec81aa130cfcfed6ae20fd6ede
rsraj9980 <91348564+rsraj9980@users.noreply.github.com>
2021-12-08 20:54:25 -0500

    MVP

--- controllers/api/appointments.js
+++ controllers/api/appointments.js
@@ -1,4 +1,4 @@
-const sgMail = require('sendgrid/mail')
+const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const Appointment = require('../../models/appointment');
 const user = require('../../models/user');
